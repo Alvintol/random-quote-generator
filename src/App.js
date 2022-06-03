6f77d2f8ce4619968f3c85e6a3ca5845721efa13
Alvin <alvin.tol@hotmail.com>
2022-06-02 19:39:07 -0600

    adds font-awesome icons script and hard code placeholder divs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 const App = () => {
   return (
-    <div className="app">
-
+    <div className='app'>
+      <div id='quote-box'>
+        <div id='text'>Random quote</div>
+        <div id='author'>-Author name</div>
+        <button id='tweet-quote'>
+          <i class='fa-brands fa-twitter'></i>
+        </button>
+        <button id='new-quote'>New Quote</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
