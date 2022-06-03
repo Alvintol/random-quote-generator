aa177ddf565434d1d53694cceb655a40bd85b872
Alvin <alvin.tol@hotmail.com>
2022-06-03 16:20:42 -0600

    cleans up UI and sets twitter link for tweet button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './styling/sass/app.scss';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [state, setState] = useState({
+    quoteList: [],
+    quote: "That's what",
+    author: 'She',
+  });
+
+  useEffect(() => {
+
+    
+  }, []);
+
+  const 
+  const getNewQuote = () => console.log('NEW QUOTE BUTTON') 
+
   return (
     <div className='app'>
       <div id='quote-box'>
-        <div id='quote-box'>
-          <div id='text'>Random quote</div>
-          <div id='author'>-Author name</div>
+        <div id='box-top'>
+          <div id='text'>
+            <p>" {state.quote} "</p>
+          </div>
+          <div id='author'>
+            <p>- {state.author}</p>
+          </div>
         </div>
         <div id='button-container'>
-          <button id='tweet-quote'>
+          <a id='tweet-quote' href='https://twitter.com/intent/tweet'>
             <i class='fa-brands fa-twitter'></i>
-          </button>
-          <button id='new-quote'>New Quote</button>
+          </a>
+          <button 
+          id='new-quote'
+          onClick={() => getNewQuote}
+          >New Quote</button>
         </div>
       </div>
     </div>
