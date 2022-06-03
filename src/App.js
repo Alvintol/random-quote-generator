b6a44b74e2418138b7b5bdbe1d28861dc2dfa076
Alvin <alvin.tol@hotmail.com>
2022-06-02 19:53:20 -0600

    styles and centers div placeholders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import './styling/sass/app.scss';
+
 const App = () => {
   return (
     <div className='app'>
       <div id='quote-box'>
-        <div id='text'>Random quote</div>
-        <div id='author'>-Author name</div>
-        <button id='tweet-quote'>
-          <i class='fa-brands fa-twitter'></i>
-        </button>
-        <button id='new-quote'>New Quote</button>
+        <div id='quote-box'>
+          <div id='text'>Random quote</div>
+          <div id='author'>-Author name</div>
+        </div>
+        <div id='button-container'>
+          <button id='tweet-quote'>
+            <i class='fa-brands fa-twitter'></i>
+          </button>
+          <button id='new-quote'>New Quote</button>
+        </div>
       </div>
     </div>
   );
